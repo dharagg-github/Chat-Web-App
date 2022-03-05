a2d16b658379fa5d3d56646499167e29044f2451
dharagg-github <100713788+dharagg-github@users.noreply.github.com>
2022-03-05 20:58:26 +0530

    full fledged login with template
    Complete UI for User login is made with
    templates

--- djangoProject/chat/urls.py
+++ djangoProject/chat/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
     path('index', views.index, name='index'),
     path('create', views.create, name='create'),
-    path('forget', views.forget, name='forget'),
     path('logout', views.logout, name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
 
 ]
--- djangoProject/chat/views.py
+++ djangoProject/chat/views.py
@@ -1,7 +1,19 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import login as auth_logout
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes,force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from . tokens import generate_token
+from django.core.mail import EmailMessage,send_mail
+
+from djangoProject import settings
+
+
 
 
 # Create your views here.
@@ -16,39 +28,119 @@ def index(request):
 
 # Requesting Sign In page
 def login(request):
+
+    if request.method == "POST":
+        Username = request.POST.get('Username')
+        pass1 = request.POST.get('pass1')
+
+        user = authenticate(username=Username, password=pass1)
+
+        if user is not None:
+            auth_login(request, user)
+            uname = user.username
+            return render(request, 'login.html', {'uname': uname})
+        else:
+            messages.error(request, "Mismatching Credentials")
+            return redirect('/index')
+
     return render(request, 'login.html')
 
-# Requesting forget password page
-@csrf_exempt
-def forget(request):
-    return render(request, "forget.html")
 
 # Requesting create account page
 @csrf_exempt
 def create(request):
-    print(request.method)
 
     if request.method == "POST":
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        Username = request.POST['Username']
-        pass1 = request.POST['pass1']
-        pass2 = request.POST['pass2']
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        Username = request.POST.get('Username')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        if User.objects.filter(username=Username):
+            messages.error(request, "Username Already Exist")
+            return redirect('/create')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "Email Already Registered!")
+            return redirect('/create')
+
+        if pass1 != pass2:
+            messages.error(request, "Password Mismatch")
+            return redirect('/create')
+
+        if not Username.isalnum():
+            messages.error(request, "Username Should Be Alpha-Numeric!")
+            return redirect('/create')
 
         myuser = User.objects.create_user(Username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
+        myuser.username = Username
+        myuser.is_active = False
 
         myuser.save()
 
-        messages.success(request, "Your Account is successfully Created")
+        messages.success(request, "Your Account is successfully Created. We have send you a confirmation Email. So,Please confirm your Email in order to Activate your Account... ")
 
+        # Welcome Email
+
+        subject = "Online Chat Room - Login"
+
+        message = "Hello " + myuser.first_name +" "+myuser.last_name + "!! \n" + "ThankYou for visiting our Website.\nWe have sent you a Confimation Email, So please confirm your Email Address to activate your Account."
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        # Email Address Confirmation
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your Email @ Online Chat Room - Login!!"
+        message2 = render_to_string('email_confirmation.html',{
+            'name': myuser.first_name + ' ' + myuser.last_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
 
         return redirect('/index')
 
 
     return render(request, "create.html")
 
+@csrf_exempt
+
 def logout(request):
-    pass
+    auth_logout(request)
+    messages.success(request, "Logged out Successfully!")
+    return redirect('/index')
+
+def activate(request, uidb64, token):
+
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        auth_login(request, myuser)
+        return redirect('/index')
+
+    else:
+        return render(request, "activation_failed.html")
+
+
+
